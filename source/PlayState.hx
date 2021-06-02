2fc9f14126354bf1847a0c0f9bfe4d8e3e6401f1
Astr0verload <85229655+Astr0verload@users.noreply.github.com>
2021-06-02 20:09:52 +0300

    gf now uses cheer animation in the tutorial

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2424,15 +2424,19 @@ class PlayState extends MusicBeatState
 			boyfriend.playAnim('idle');
 		}
 
-		if (curBeat % 8 == 7 && curSong == 'Bopeebo')
+		switch(SONG.song) // Mid Song Events
 		{
-			boyfriend.playAnim('hey', true);
-		}
-
-		if (curBeat % 16 == 15 && SONG.song == 'Tutorial' && dad.curCharacter == 'gf' && curBeat > 16 && curBeat < 48)
-		{
-			boyfriend.playAnim('hey', true);
-			dad.playAnim('cheer', true);
+			case 'Tutorial':
+				if (curBeat % 16 == 15 && dad.curCharacter == 'gf' && curBeat > 16 && curBeat < 48)
+				{
+					boyfriend.playAnim('hey', true);
+					dad.playAnim('cheer', true);
+				}
+			case 'Bopeebo':
+				if (curBeat % 8 == 7 && curSong == 'Bopeebo')
+				{
+					boyfriend.playAnim('hey', true);
+				}
 		}
 
 		switch (curStage)
